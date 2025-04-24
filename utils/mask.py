57bcd73f66d9419f8dfb745b0a044e0bbaf83f40
liu <3330176274@qq.com>
2025-04-24 13:27:25 +0800

    modify the mask bug2

--- utils/mask.py
+++ utils/mask.py
@@ -27,7 +27,7 @@ def create_mask(inp, targ, pad=0):
     # decoder's second attention block(encoder-decoder attention) 使用的padding create_mask
     # 【注意】：这里的mask是用于遮挡encoder output的填充pad，而encoder的输出与其输入shape都是[b,inp_seq_len,d_model]
     # 所以这里mask的长度是inp_seq_len而不是targ_mask_len
-    dec_padding_mask = create_padding_mask(targ, pad)  # =>[b,1,1,inp_seq_len] mask=1的位置为pad
+    dec_padding_mask = create_padding_mask(inp, pad)  # =>[b,1,1,inp_seq_len] mask=1的位置为pad
 
     return enc_padding_mask, combined_mask, dec_padding_mask
     # [b,1,1,inp_seq_len], [b,1,targ_seq_len,targ_seq_len], [b,1,1,inp_seq_len]
